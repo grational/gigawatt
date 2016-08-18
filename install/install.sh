d0a20f0cc7178c7bbd2ad2f8a5b380c0134a28ba
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2016-08-18 17:25:29 +0200

    installation script update

--- install/install.sh
+++ install/install.sh
@@ -27,17 +27,24 @@ install() {
 		fi
 	done
 
+	# check for an already present gigawatt executable
+	local gw_check="$(command -v gigawatt || echo none)"
+	if [[ $gw_check != none ]]; then
+		exitmsg "It seems that you already have a gigawatt executable: '${gw_check}'.\nAborting installation."
+	fi
+
+	# abort installation for existing default dir
+	[[ -d $inst_dir ]] && \
+		exitmsg 'Seems that you have already installed gigawatt. Update using:\n  cd ~/bin/github/gigawatt && git pull'
+
 	# create and add local ~/bin to PATH if needed
 	if [[ ! ${PATH} =~ $user_bin ]]; then
 		printf '# gigawatt install script\nexport PATH="${HOME}/bin:${PATH}"' >> "${shell_cfg}"
 		[[ ! -d $user_bin ]] && mkdir "${user_bin}"
 	fi
 	[[ ! -d ${ghub_dir} ]] && mkdir "${ghub_dir}"
-	# abort installation if a gigawatt 
-	[[ -d $inst_dir ]] && \
-		exitmsg 'Seems that you have already installed gigawatt. Update using:\n  cd ~/bin/github/gigawatt && git pull'
 	git clone 'https://github.com/grecovery/gigawatt' "$inst_dir"
-	ln -s "$inst_dir/gigawatt" ~/bin/
+	ln -s "$inst_dir/gigawatt" "$user_bin"
 }
 
 install
